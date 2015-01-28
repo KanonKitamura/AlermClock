471e6052d6de7573f9ff6c657e5ef665b521e953
KanonKitamura <kanonkitamura0405@gmail.com>
2015-01-28 16:59:34 +0900

    Initial Commit

---
+++ AlermClock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlermClock
+//
+//  Created by kanon kitamura on 2015/01/28.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlermClock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlermClock
+//
+//  Created by kanon kitamura on 2015/01/28.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
